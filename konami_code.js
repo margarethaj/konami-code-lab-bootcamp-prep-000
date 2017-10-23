ad06c058a762ab8c51fc0db7bf54d2b4cb239221
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-23 16:21:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,9 +5,19 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
 
-  function createSpy(){
-    alert('spy created')
-  }
+  $(document).addEventListener('keydown', (e) => {
+    const key = event.key;
+    if(key == code[index]){
+      index++;
+      if(index == code.length){
+        window.alert("spy created");
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  });
 
   function onKeyDownHandler(e){
     const key = parseInt(e.detail || e.which || e.detail || e.location);
